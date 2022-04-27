76d8c7a7d356f877674d9d4a1809e1acd1d8c226
Devin Tripp <devinshawntripp@gmail.com>
2022-04-27 16:08:39 -0500

    fixing the address

--- src/components/NFTDetails.js
+++ src/components/NFTDetails.js
@@ -1,82 +1,81 @@
-import React, {useState, useEffect, useContext} from 'react'
-import {Col} from 'react-bootstrap'
-import { SocketContext } from '../contexts/socket'
-import axios from 'axios'
-import { useSelector } from 'react-redux'
+import React, { useState, useEffect, useContext } from "react";
+import { Col } from "react-bootstrap";
+import { SocketContext } from "../contexts/socket";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
+export default function NFTDetails({ tokenId, imgURL, address, align }) {
+  const app = useSelector((state) => state.app);
+  const [owner, setOwner] = useState("");
+  const [hovering, setHovering] = useState(false);
+  const [nftData, setNftData] = useState({});
+  const [findAddress, setFindAddr] = useState("");
+  const socket = useContext(SocketContext);
 
+  const hoverNFT = {
+    border: hovering ? "3px solid blue" : "",
+  };
 
-export default function NFTDetails({tokenId, imgURL, address, align}) {
+  const handleMouseOver = () => {
+    setHovering(true);
+  };
 
+  const handleMouseLeave = () => {
+    setHovering(false);
+  };
 
-    const app = useSelector((state) => state.app)
-    const [owner,setOwner] = useState("");
-    const [hovering, setHovering] = useState(false)
-    const [nftData, setNftData] = useState({})
-    const [findAddress, setFindAddr] = useState("")
-    const socket = useContext(SocketContext)
+  useEffect(() => {
+    const getNfts = async () => {
+      await axios
+        .get(`${process.env.REACT_APP_URL}/api/nft/${tokenId}`)
+        .then((res) => {
+          setNftData(res.data);
+        });
+      var own = await app.alienLock.methods.getAddressOfTokenId(tokenId).call();
+      if (own === "0x0000000000000000000000000000000000000000") {
+        console.log("The owner eqauls null");
+        own = await app.angryAliens.methods.ownerOf(tokenId).call();
+      }
+      console.log("OWNER IS: ", own);
 
-    const hoverNFT = {
-        border: hovering ? "3px solid blue" : ""
-    }
+      setOwner(own);
+    };
+    getNfts();
+    // const handler = (data) => {
+    //     console.log(data)
+    //     setNftData(data);
+    // }
 
-    const handleMouseOver = () => {
-        setHovering(true);
-    }
+    // socket.emit('sendNftId', tokenId);
 
-    const handleMouseLeave = () => {
-        setHovering(false)
-    }
+    // socket.on('getNftData', handler)
 
-    useEffect(() => {
+    // return () => {
+    //     socket.off('getNftData')
+    // }
+  }, []);
 
-        const getNfts = async () => {
-            await axios.get(`${process.env.REACT_APP_URL}/api/nft/${tokenId}`).then((res) => {
-                setNftData(res.data)
-            })
-            var own = await app.alienLock.methods.getAddressOfTokenId(tokenId).call();
-            console.log("OWNER IS: ", own)
-
-            setOwner(own);
-        }
-        getNfts();
-        // const handler = (data) => {
-        //     console.log(data)
-        //     setNftData(data);
-        // }
-
-        // socket.emit('sendNftId', tokenId);
-
-        // socket.on('getNftData', handler)
-
-        // return () => {
-        //     socket.off('getNftData')
-        // }
-    },[])
-
-
-    return (
-        <div onMouseOver={handleMouseOver} style={hoverNFT} onMouseLeave={handleMouseLeave}>
-            <img 
-                src={`${imgURL}${tokenId}.png`}
-                alt="Angry Alien"
-                className='nftsowned'
-            />
-            <ul style={{textAlign: align ? align : "left", listStyle: "none"}}>
-                {/* <li>Owner: {address.slice(0, 5) + '...' + address.slice(38, 42)}</li> */}
-                <li>Owner: {owner.slice(0,5) + '...' + owner.slice(38,42)}</li>
-                <li>Type: {nftData.type}</li>
-                <li>Level: {nftData.level}</li>
-                <li>Battles Won: {nftData.battlesWon}</li>
-                <li>Battles Lost: {nftData.battlesLost}</li>
-                <li>Rarity: {Math.round(nftData.rarity)}</li>
-                <li>Rank: {nftData.rank}</li>
-
-            </ul>
-        </div>
-        
-        
-    )
-
-
+  return (
+    <div
+      onMouseOver={handleMouseOver}
+      style={hoverNFT}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img
+        src={`${imgURL}${tokenId}.png`}
+        alt="Angry Alien"
+        className="nftsowned"
+      />
+      <ul style={{ textAlign: align ? align : "left", listStyle: "none" }}>
+        {/* <li>Owner: {address.slice(0, 5) + '...' + address.slice(38, 42)}</li> */}
+        <li>Owner: {owner.slice(0, 5) + "..." + owner.slice(38, 42)}</li>
+        <li>Type: {nftData.type}</li>
+        <li>Level: {nftData.level}</li>
+        <li>Battles Won: {nftData.battlesWon}</li>
+        <li>Battles Lost: {nftData.battlesLost}</li>
+        <li>Rarity: {Math.round(nftData.rarity)}</li>
+        <li>Rank: {nftData.rank}</li>
+      </ul>
+    </div>
+  );
 }
--- src/components/NewHome.js
+++ src/components/NewHome.js
@@ -1,228 +1,260 @@
-import react, {useEffect, useState} from 'react';
-import {Row, Col, Spinner} from 'react-bootstrap'
-import Countdown from 'react-countdown'
-import { useHorizontalScroll } from '../hooks/useSideScroll'
-import NFTDetails from './NFTDetails'
+import react, { useEffect, useState } from "react";
+import { Row, Col, Spinner } from "react-bootstrap";
+import Countdown from "react-countdown";
+import { useHorizontalScroll } from "../hooks/useSideScroll";
+import NFTDetails from "./NFTDetails";
 
-import { loadAppData } from '../store/slices/app-slice';
-
-
-
+import { loadAppData } from "../store/slices/app-slice";
 
 // Import Images + CSS
-import twitter from '../images/socials/twitter.svg'
-import instagram from '../images/socials/instagram.svg'
-import opensea from '../images/socials/opensea.svg'
-import showcase from '../images/showcase.jpg'
-import '../App.css'
-
-
+import twitter from "../images/socials/twitter.svg";
+import instagram from "../images/socials/instagram.svg";
+import opensea from "../images/socials/opensea.svg";
+import showcase from "../images/showcase.jpg";
+import "../App.css";
 
 // Import ABI + Config
-import config from '../config.json'
+import config from "../config.json";
 
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
- 
-
-
-
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 export default function NewHome() {
+  const [counter, setCounter] = useState(7);
+  const [isCycling, setIsCycling] = useState(false);
+  const [currentTime, setCurrentTime] = useState(new Date().getTime());
 
+  const [isMinting, setIsMinting] = useState(false);
 
-    const [counter, setCounter] = useState(7)
-    const [isCycling, setIsCycling] = useState(false)
-    const [currentTime, setCurrentTime] = useState(new Date().getTime())
+  const app = useSelector((state) => state.app);
+  const dispatch = useDispatch();
 
+  const cycleImages = async () => {
+    const getRandomNumber = () => {
+      const counter = Math.floor(Math.random() * 6000) + 1;
+      setCounter(counter);
+    };
 
-    const [isMinting, setIsMinting] = useState(false);
-     
-    const app = useSelector((state) => state.app);
-    const dispatch = useDispatch()
+    if (!isCycling) {
+      setInterval(getRandomNumber, 3000);
+    }
+    setIsCycling(true);
+  };
 
-    
+  useEffect(() => {
+    cycleImages();
+  }, []);
 
-    const cycleImages = async () => {
-		const getRandomNumber = () => {
-			const counter = (Math.floor(Math.random() * 6000)) + 1
-			setCounter(counter)
-		}
+  const mintNFTHandler = async () => {
+    if (app.revealTime > new Date().getTime()) {
+      window.alert("Minting is not live yet!");
+      return;
+    }
 
-		if (!isCycling) { setInterval(getRandomNumber, 3000) }
-		setIsCycling(true)
-	}
+    if (app.tokensOwned.length > 10) {
+      window.alert("You've already minted!");
+      return;
+    }
 
-    useEffect(() => {
-        
-        cycleImages()
-    }, [])
+    // Mint NFT
+    if (app.angryAliens && app.address) {
+      setIsMinting(true);
+      // setIsError(false)
 
+      await app.angryAliens.methods
+        .mint(1)
+        .send({ from: app.address, value: 60000000000000000 })
+        .on("confirmation", async () => {
+          const maxSupply = await app.angryAliens.methods.maxSupply().call();
+          const totalSupply = await app.angryAliens.methods
+            .totalSupply()
+            .call();
+          // setSupplyAvailable(maxSupply - totalSupply)
 
-    const mintNFTHandler = async () => {
-		if (app.revealTime > new Date().getTime()) {
-			window.alert('Minting is not live yet!')
-			return
-		}
+          const ownerOf = await app.angryAliens.methods
+            .walletOfOwner(app.address)
+            .call();
+          // setOwnerOf(ownerOf)
+          // setTokensOwned(await angryAliens.methods.walletOfOwner(account).call())
+          dispatch(
+            loadAppData({
+              ...app,
+              supplyAvailable: maxSupply - totalSupply,
+              tokensOwned: ownerOf,
+            })
+          );
+        })
+        .on("error", (error) => {
+          window.alert(error);
+          // setIsError(true)
+        });
+    }
 
-		if (app.tokensOwned.length > 10) {
-			window.alert('You\'ve already minted!')
-			return
-		}
+    setIsMinting(false);
+  };
 
-		// Mint NFT
-		if (app.angryAliens && app.address) {
-			setIsMinting(true)
-			// setIsError(false)
-            
+  const scrollRef = useHorizontalScroll();
 
-			await app.angryAliens.methods.mint(1).send({ from: app.address, value: 0 })
-				.on('confirmation', async () => {
-					const maxSupply = await app.angryAliens.methods.maxSupply().call()
-					const totalSupply = await app.angryAliens.methods.totalSupply().call()
-					// setSupplyAvailable(maxSupply - totalSupply)
+  var tokenImageURI = `https://aliensnft.mypinata.cloud/ipfs/Qmcrwm55jLALriGzhGopo6tiZRGWekmdhmg2f41kJV4ag4/`;
 
-					const ownerOf = await app.angryAliens.methods.walletOfOwner(app.address).call()
-					// setOwnerOf(ownerOf)
-					// setTokensOwned(await angryAliens.methods.walletOfOwner(account).call())
-                    dispatch(loadAppData({...app, supplyAvailable: maxSupply-totalSupply, tokensOwned: ownerOf }))
-				})
-				.on('error', (error) => {
-					window.alert(error)
-					// setIsError(true)
-				})
-		}
+  return (
+    <div>
+      <main>
+        <section id="welcome" className="welcome">
+          <Row className="header my-3 p-3 mb-0 pb-0">
+            <Col xs={12} md={12} lg={8} xxl={8}>
+              <h1>Angry Aliens</h1>
+              <p className="sub-header">Availble on 04 / 10 / 22</p>
+            </Col>
+            <Col className="flex social-icons">
+              <a
+                href="https://twitter.com/AngryAliensproj"
+                target="_blank"
+                className="circle flex button"
+              >
+                <img src={twitter} alt="Twitter" />
+              </a>
+              <a href="#" target="_blank" className="circle flex button">
+                <img src={instagram} alt="Instagram" />
+              </a>
+              <a
+                href={`${app.openseaURL}/collection/${config.PROJECT_NAME}`}
+                target="_blank"
+                className="circle flex button"
+              >
+                <img src={opensea} alt="Opensea" />
+              </a>
+            </Col>
+          </Row>
 
-		setIsMinting(false)
-	}
+          <Row className="flex m-3">
+            <Col md={5} lg={4} xl={5} xxl={4} className="text-center">
+              <img
+                src={`https://aliensnft.mypinata.cloud/ipfs/Qmcrwm55jLALriGzhGopo6tiZRGWekmdhmg2f41kJV4ag4/${counter}.png`}
+                alt="Angry Aliens"
+                className="showcase"
+              />
+            </Col>
+            <Col md={5} lg={4} xl={5} xxl={4}>
+              {app.revealTime !== 0 && (
+                <Countdown
+                  date={currentTime + (app.revealTime - currentTime)}
+                  className="countdown mx-3"
+                />
+              )}
+              <p className="text">
+                Angry Aliens is a project with multiple utilities that will be
+                mostly community directed. That is all soley coded by yours
+                truly -MOJE BAKE SAKETORO
+              </p>
+              <a href="#about" className="button mx-3">
+                Learn More!
+              </a>
+            </Col>
+          </Row>
+        </section>
+        <section id="about" className="about">
+          <Row className="flex m-3">
+            <h2 className="text-center p-3">About the Collection</h2>
+            <Col md={5} lg={4} xl={5} xxl={4} className="text-center">
+              <img
+                src={showcase}
+                alt="Multiple Angry Aliens"
+                className="showcase"
+              />
+            </Col>
+            <Col md={5} lg={4} xl={5} xxl={4}>
+              {app.isError ? (
+                <p>{app.message}</p>
+              ) : (
+                <div>
+                  <h3>Mint your NFT in</h3>
+                  {app.revealTime !== 0 && (
+                    <Countdown
+                      date={currentTime + (app.revealTime - currentTime)}
+                      className="countdown"
+                    />
+                  )}
+                  <ul>
+                    <li>10,000 generated images using an art generator</li>
+                    <li>
+                      Free minting on Rinkeby testnet/ Actual chain tbd by
+                      community vote most likely cronos
+                    </li>
+                    <li>Viewable on Opensea/Ebuisbay shortly after minting</li>
+                  </ul>
 
+                  {isMinting ? (
+                    <Spinner animation="border" className="p-3 m-2" />
+                  ) : (
+                    <button
+                      onClick={mintNFTHandler}
+                      className="button mint-button mt-3"
+                    >
+                      Mint
+                    </button>
+                  )}
 
-    const scrollRef = useHorizontalScroll();
+                  {app.tokensOwned.length > 0 && (
+                    <p>
+                      <small>
+                        View your NFT on
+                        <a
+                          href={`${app.openseaURL}/assets/${app.angryAliens._address}/${app.tokensOwned[0]}`}
+                          target="_blank"
+                          style={{ display: "inline-block", marginLeft: "3px" }}
+                        >
+                          OpenSea
+                        </a>
+                      </small>
+                    </p>
+                  )}
+                </div>
+              )}
+            </Col>
+          </Row>
 
-    var tokenImageURI = `https://aliensnft.mypinata.cloud/ipfs/Qmcrwm55jLALriGzhGopo6tiZRGWekmdhmg2f41kJV4ag4/`;
-    
+          <Row style={{ marginTop: "100px" }}>
+            <Col>
+              {app.angryAliens && (
+                <a
+                  href={`${app.explorerURL}/address/${app.angryAliens._address}`}
+                  target="_blank"
+                  className="text-center"
+                >
+                  {app.angryAliens._address}
+                </a>
+              )}
+            </Col>
+          </Row>
+        </section>
 
-    return (
-        <div>
-            <main>
-                <section id='welcome' className='welcome'>
-                    <Row className='header my-3 p-3 mb-0 pb-0'>
-                            <Col xs={12} md={12} lg={8} xxl={8}>
-                                <h1>Angry Aliens</h1>
-                                <p className='sub-header'>Availble on 04 / 10 / 22</p>
-                            </Col>
-                            <Col className='flex social-icons'>
-                                <a
-                                    href="https://twitter.com/AngryAliensproj"
-                                    target='_blank'
-                                    className='circle flex button'>
-                                    <img src={twitter} alt="Twitter" />
-                                </a>
-                                <a
-                                    href="#"
-                                    target='_blank'
-                                    className='circle flex button'>
-                                    <img src={instagram} alt="Instagram" />
-                                </a>
-                                <a
-                                    href={`${app.openseaURL}/collection/${config.PROJECT_NAME}`}
-                                    target='_blank'
-                                    className='circle flex button'>
-                                    <img src={opensea} alt="Opensea" />
-                                </a>
-                            </Col>
-                        </Row>
-
-                        <Row className='flex m-3'>
-                            <Col md={5} lg={4} xl={5} xxl={4} className='text-center'>
-                                <img
-                                    src={`https://aliensnft.mypinata.cloud/ipfs/Qmcrwm55jLALriGzhGopo6tiZRGWekmdhmg2f41kJV4ag4/${counter}.png`}
-                                    alt="Angry Aliens"
-                                    className='showcase'
-                                />
-                            </Col>
-                            <Col md={5} lg={4} xl={5} xxl={4}>
-                                {app.revealTime !== 0 && <Countdown date={currentTime + (app.revealTime - currentTime)} className='countdown mx-3' />}
-                                <p className='text'>
-                                    Angry Aliens is a project with multiple utilities that will be mostly community directed. That is all soley coded by yours truly -MOJE BAKE SAKETORO
-                                </p>
-                                <a href="#about" className='button mx-3'>Learn More!</a>
-                            </Col>
-                        </Row>
-                </section>
-                <section id='about' className='about'>
-
-                        <Row className='flex m-3'>
-                            <h2 className='text-center p-3'>About the Collection</h2>
-                            <Col md={5} lg={4} xl={5} xxl={4} className='text-center'>
-                                <img src={showcase} alt="Multiple Angry Aliens" className='showcase' />
-                            </Col>
-                            <Col md={5} lg={4} xl={5} xxl={4}>
-                                {app.isError ? (
-                                    <p>{app.message}</p>
-                                ) : (
-                                    <div>
-                                        <h3>Mint your NFT in</h3>
-                                        {app.revealTime !== 0 && <Countdown date={currentTime + (app.revealTime - currentTime)} className='countdown' />}
-                                        <ul>
-                                            <li>10,000 generated images using an art generator</li>
-                                            <li>Free minting on Rinkeby testnet/ Actual chain tbd by community vote most likely cronos</li>
-                                            <li>Viewable on Opensea/Ebuisbay shortly after minting</li>
-                                        </ul>
-
-                                        {isMinting ? (
-                                            <Spinner animation="border" className='p-3 m-2' />
-                                        ) : (
-                                            <button onClick={mintNFTHandler} className='button mint-button mt-3'>Mint</button>
-                                        )}
-
-                                        {app.tokensOwned.length > 0 &&
-                                            <p><small>View your NFT on
-                                                <a
-                                                    href={`${app.openseaURL}/assets/${app.angryAliens._address}/${app.tokensOwned[0]}`}
-                                                    target='_blank'
-                                                    style={{ display: 'inline-block', marginLeft: '3px' }}>
-                                                    OpenSea
-                                                </a>
-                                            </small></p>}
-                                    </div>
-                                )}
-                            </Col>
-                        </Row>
-
-                        <Row style={{ marginTop: "100px" }}>
-                            <Col>
-                                {app.angryAliens &&
-                                    <a
-                                        href={`${app.explorerURL}/address/${app.angryAliens._address}`}
-                                        target='_blank'
-                                        className='text-center'>
-                                        {app.angryAliens._address}
-                                    </a>
-                                }
-                            </Col>
-                        </Row>
-
-                    </section>
-
-                    <section id='about' className='about'>
-                    
-                        
-                        <Row style={{ justifyContent: "center", alignItems: "center", marginTop: "100px" }}>
-                            <h2 style={{ textAlign: "center"}}>NFTS YOU OWN</h2>
-                            <Col ref={scrollRef} className="nfts">
-                                {app.tokensOwned &&
-                                    app.tokensOwned.map((token, id) => {
-                                        // return <div onClick={(e) => handleNFTClick(e, token)}>
-                                            return <NFTDetails tokenId={token} imgURL={tokenImageURI} address={app.address} />
-                                        // </div>
-                                    })
-                                    
-                                }
-                            </Col>
-                        </Row>
-                    </section>
-            </main>
-        </div>
-    )
+        <section id="about" className="about">
+          <Row
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "100px",
+            }}
+          >
+            <h2 style={{ textAlign: "center" }}>NFTS YOU OWN</h2>
+            <Col ref={scrollRef} className="nfts">
+              {app.tokensOwned &&
+                app.tokensOwned.map((token, id) => {
+                  // return <div onClick={(e) => handleNFTClick(e, token)}>
+                  return (
+                    <NFTDetails
+                      tokenId={token}
+                      imgURL={tokenImageURI}
+                      address={app.address}
+                    />
+                  );
+                  // </div>
+                })}
+            </Col>
+          </Row>
+        </section>
+      </main>
+    </div>
+  );
 }
